a0df860f3ef1baa421dcd4291666330f9e7141c2
chuckderosier <chuckderosier2018@gmail.com>
2018-11-14 12:09:29 -0500

    react installed

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+// import { BrowserRouter as Router, Route, Switch, Link,Redirect } from 'react-router-dom'
+// import styled from 'styled-components'
+// import HomePage from './components/HomePage'
+// import SomePage from './components/SomePage'
+// import AnotherPage from './components/AnotherPage'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>App</h1>
+      </div>
+    );
+  }
+}
+
+export default App
