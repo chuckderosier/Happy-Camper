f946c6d8874d4e5d0a889214b3a66d2acf8760d6
chuckderosier <chuckderosier2018@gmail.com>
2018-11-19 10:07:39 -0500

    build removed from git ignore

---
+++ client/build/precache-manifest.2c75740c51f49740fad77a7360a6255b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "50a4832aabfc928cfeb3",
+    "url": "/static/js/main.50a4832a.chunk.js"
+  },
+  {
+    "revision": "90b94bed612b169f7f96",
+    "url": "/static/js/1.90b94bed.chunk.js"
+  },
+  {
+    "revision": "42322871a7a2070393e08085d00f29e5",
+    "url": "/index.html"
+  }
+];
