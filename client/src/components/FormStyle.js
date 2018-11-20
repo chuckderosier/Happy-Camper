d06d673fb37723899e99f0d8dffeb54f565a6763
chuckderosier <chuckderosier2018@gmail.com>
2018-11-19 21:05:00 -0500

    start styles for forms

---
+++ client/src/components/FormStyle.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const StyledForm = styled.form`
+    background-color: black;
+`
+
+class FormStyle extends Component {
+    render() {
+        return (
+            <StyledForm>
+                
+            </StyledForm>
+        )
+    }
+}
+
+export default FormStyle
--- client/src/components/UpdateUserForm.js
+++ client/src/components/UpdateUserForm.js
@@ -1,5 +1,34 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import styled from 'styled-components'
+// import { StyledForm } from './FormStyle'
+
+// const FormContainer = styled.StyledForm`
+// `
+
+const FormContainer = styled.div`
+    background-image: url("/images/happyCamperBG3.jpg");
+    background-size: cover;
+    background-repeat: no-repeat;
+    width: 100vw;
+    height: 100vh;
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+    form {
+        margin: .3em auto;
+        width: 40vw;
+        height: 30vh;
+        background-color: rgba(255,255,255,.5);
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    div {
+        display: flex;
+        align-self: space-between;
+    }
+`
 
 class UpdateForm extends Component {
 
@@ -28,32 +57,35 @@ class UpdateForm extends Component {
 
     render() {
         return (
-            <div>
+            <FormContainer>
                 <form onSubmit={this.handleUpdate}>
                     <div>
                         <label htmlFor="username">User Name: </label>
                         <input
                         onChange={this.handleChange}
                         value={this.state.user.username}
-                        type="text" name="username" />
+                        type="text" name="username"
+                        maxlength="20" />
                     </div>
                     <div>
                         <label htmlFor="campingStyle">Camping Style: </label>
                         <input
                         onChange={this.handleChange}
                         value={this.state.user.campingStyle}
-                        type="text" name="campingStyle" />
+                        type="text" name="campingStyle"
+                        maxlength="20" />
                     </div>
                     <div>
                         <label htmlFor="userState">State You Live In: </label>
                         <input
                         onChange={this.handleChange}
                         value={this.state.user.userState}
-                        type="text" name="userState" />
+                        type="text" name="userState"
+                        maxlength="20" />
                     </div>
                     <button type="submit">Save changes</button>
                 </form>
-            </div>
+            </FormContainer>
         )
     }
 }
