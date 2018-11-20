a5300e12f9f0b2fbc8f7840969acb835278c9557
chuckderosier <chuckderosier2018@gmail.com>
2018-11-20 11:43:03 -0500

    all styling done except media query

--- client/src/components/CampsiteForm.js
+++ client/src/components/CampsiteForm.js
@@ -1,7 +1,51 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-// import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 
+const FormContainer = styled.div`
+    background-image: url("/images/happyCamperBG3.jpg");
+    background-size: cover;
+    background-repeat: no-repeat;
+    width: 100vw;
+    height: 100vh;
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+    form {
+        margin: .3em auto;
+        padding: 15px;
+        width: 40vw;
+        height: 70vh;
+        background-color: rgba(255,255,255,.5);
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+    }
+    div {
+        margin: 0 auto;
+    }
+    button {
+        margin: 0 auto;
+        font-family: "Times New Roman", Times, serif;
+        color: rgb(0,75,0);
+        font-size: 1em;
+        background-color: rgb(200,255,200);
+        border: brown solid 4px;
+        border-radius: 25em;     
+    }
+    .userLinks {
+        margin: 0 auto;
+        padding: 5px;
+        font-family: "Times New Roman", Times, serif;
+        color: brown;
+        font-size: 1em;
+        text-decoration: none;
+        background-color: rgb(255,235,210);
+        border: rgb(0,75,0) solid 4px;
+        border-radius: 25em; 
+    }
+`
 
 class CampsiteForm extends Component {
 
@@ -34,74 +78,79 @@ class CampsiteForm extends Component {
 
     render() {
         return (
-            <div>
+            <FormContainer>
                 <form onSubmit={this.handleSubmit}>
                     <div>
                         <label htmlFor="campsiteName">Campsite Name: </label>
                         <input
-                        onChange={this.handleChange}
-                        value={this.state.newCampsite.campsiteName}
-                        type="text" name="campsiteName"
-                        placeholder="Campsite Name" />
+                            onChange={this.handleChange}
+                            value={this.state.newCampsite.campsiteName}
+                            type="text" name="campsiteName"
+                            placeholder="Campsite Name" />
                     </div>
                     <div>
                         <label htmlFor="campsiteLocation">Campsite location: </label>
                         <input
-                        onChange={this.handleChange}
-                        value={this.state.newCampsite.campsiteLocation}
-                        type="text" name="campsiteLocation"
-                        placeholder="State, Park Name, Part of State" />
+                            onChange={this.handleChange}
+                            value={this.state.newCampsite.campsiteLocation}
+                            type="text" name="campsiteLocation"
+                            placeholder="State, Park Name, Part of State" />
                     </div>
                     <div>
                         <label htmlFor="campsiteType">Camping types: </label>
                         <input
-                        onChange={this.handleChange}
-                        value={this.state.newCampsite.campsiteType}
-                        type="text" name="campsiteType"
-                        placeholder="ie, Cabin, RV, tent, primitive" />
+                            onChange={this.handleChange}
+                            value={this.state.newCampsite.campsiteType}
+                            type="text" name="campsiteType"
+                            placeholder="ie, Cabin, RV, tent, primitive" />
                     </div>
                     <div>
                         <label htmlFor="campsiteOpen">Campsite is open: </label>
                         <input
-                        onChange={this.handleChange}
-                        value={this.state.newCampsite.campsiteOpen}
-                        type="text" name="campsiteOpen"
-                        placeholder="Year Round, Closed, Spring, Summer, Fall" />
+                            onChange={this.handleChange}
+                            value={this.state.newCampsite.campsiteOpen}
+                            type="text" name="campsiteOpen"
+                            placeholder="Year Round, Closed, Spring, Summer, Fall" />
                     </div>
                     <div>
                         <label htmlFor="campsiteDescription">Campsite description: </label>
                         <input
-                        onChange={this.handleChange}
-                        value={this.state.newCampsite.campsiteDescription}
-                        type="text" name="campsiteDescription"
-                        placeholder="Your thoughts on the camp" />
+                            onChange={this.handleChange}
+                            value={this.state.newCampsite.campsiteDescription}
+                            type="text" name="campsiteDescription"
+                            placeholder="Your thoughts on the camp" />
                     </div>
                     <div>
                         <label htmlFor="campsiteActivities">Campsite activities: </label>
                         <input onChange={this.handleChange}
-                        value={this.state.newCampsite.campsiteActivities}
-                        type="text" name="campsiteActivities"
-                        placeholder="Swimming, Hiking, Boating, Fishing, Hunting, Education" />
+                            value={this.state.newCampsite.campsiteActivities}
+                            type="text" name="campsiteActivities"
+                            placeholder="Swimming, Hiking, Boating, Fishing, Hunting, Education" />
                     </div>
                     <div>
                         <label htmlFor="campsiteLinkToBook">Link to book campsite: </label>
                         <input
-                        onChange={this.handleChange}
-                        value={this.state.newCampsite.campsiteLinkToBook}
-                        type="text" name="campsiteLinkToBook"
-                        placeholder="Link to booking website" />
+                            onChange={this.handleChange}
+                            value={this.state.newCampsite.campsiteLinkToBook}
+                            type="text" name="campsiteLinkToBook"
+                            placeholder="Link to booking website" />
                     </div>
                     <div>
                         <label htmlFor="campsiteImg">Link to image of campsite: </label>
                         <input
-                        onChange={this.handleChange}
-                        value={this.state.newCampsite.campsiteImg}
-                        type="text" name="campsiteImg"
-                        placeholder="Link to an image of the campsite" />
+                            onChange={this.handleChange}
+                            value={this.state.newCampsite.campsiteImg}
+                            type="text" name="campsiteImg"
+                            placeholder="Link to an image of the campsite" />
                     </div>
                     <button type="submit">Create Campsite</button>
+                    <div>
+                        <Link
+                            to={`/users/${this.props.match.params.userId}`}
+                            className="userLinks" >Your Campsite Page</Link>
+                    </div>
                 </form>
-            </div>
+            </FormContainer>
         )
     }
 }
