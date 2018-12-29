416999fa0631ebd196c73ea7165fa66412202ac1
chuckderosier <chuckderosier2018@gmail.com>
2018-12-29 12:16:12 -0500

    branch to fix heroku

--- client/src/components/UpdateCampsiteForm.js
+++ client/src/components/UpdateCampsiteForm.js
@@ -14,7 +14,7 @@ const FormContainer = styled.div`
     align-items: center;
     form {
         margin: .3em auto;
-        padding: 15px;
+        padding: 12px;
         width: 50vw;
         height: 90vh;
         background-color: rgba(255,255,255,.5);
