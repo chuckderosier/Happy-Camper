2c1d406bd805426d3261da97b1df22a54e771c96
chuckderosier <chuckderosier2018@gmail.com>
2019-03-09 18:46:47 -0500

    changed node version in package

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
 
 app.use('/', routes)
 
-app.listen(process.env.PORT || 3001, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 })
 // const PORT = process.env.PORT || 3001
