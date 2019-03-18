2dba96818d0864dd82cf3a642cbc289ae78530ac
chuckderosier <chuckderosier2018@gmail.com>
2019-03-18 15:21:08 -0400

    removed / from server

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const routes = require('./routes/index')
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.use(express.static(__dirname + '/client/build/'))
+app.use(express.static(__dirname + 'client/build/'))
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/client/build/index.html')
+  res.sendFile(__dirname + 'client/build/index.html')
 })
 
 app.use('/', routes)
