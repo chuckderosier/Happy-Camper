bfd98747826e328ee980b1744e1016350e9f5e9f
chuckderosier <chuckderosier2018@gmail.com>
2019-04-23 14:48:13 -0400

    added api to server js path

--- server.js
+++ server.js
@@ -5,20 +5,15 @@ const routes = require('./routes/index')
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.use(express.static(__dirname + '/client/build/'))
+app.use(express.static(__dirname + 'api/client/build/'))
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/client/build/index.html')
+  res.sendFile(__dirname + 'api/client/build/index.html')
 })
 
 app.use('/', routes)
 
-// app.listen(process.env.PORT || 3000, function(){
-//   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-// })
-
-// const PORT = process.env.PORT || config.httpPort
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3000
 app.listen(port, () => {
   console.log(`Server is listening on PORT: ${port}`)
 })
