a9e3370b4ed1bde769eaff7b9b2c2626624b8427
chuckderosier <chuckderosier2018@gmail.com>
2019-04-26 14:27:57 -0400

    modified app.get in server

--- server.js
+++ server.js
@@ -7,13 +7,19 @@ app.use(express.json())
 
 app.use(express.static(__dirname + '/client/build/'))
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/client/build/index.html')
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/client/build/index.html')
+// })
+
+app.get('/:language(' + content.languageSelector + ')/:page', function (req, res) {
+    res.header("Cache-Control", "no-cache, no-store, must revalidate")
+    res.header("Pragma", "no-cache")
+    res.header("Expires", 0)
 })
 
 app.use('/', routes)
 
-app.disable('etag')
+// app.disable('etag')
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
