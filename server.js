966c271e416f48b663ef5422ade485b830b43ac8
chuckderosier <chuckderosier2018@gmail.com>
2019-04-27 17:27:44 -0400

    switched / to *

--- server.js
+++ server.js
@@ -7,19 +7,11 @@ app.use(express.json())
 
 app.use(express.static(__dirname + '/client/build/'))
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(__dirname + '/client/build/index.html')
 })
 
-// app.get('/:language(' + content.languageSelector + ')/:page', function (req, res) {
-//     res.header("Cache-Control", "no-cache, no-store, must revalidate")
-//     res.header("Pragma", "no-cache")
-//     res.header("Expires", 0)
-// })
-
 app.use('/', routes)
-
-// app.disable('etag')
 
 const port = process.env.PORT || 3001
 app.listen(port, () => {
