e324b4d0e41b820c1abb2f35372456ed663c44e4
chuckderosier <chuckderosier2018@gmail.com>
2019-05-08 15:52:22 -0400

    trouble shoot server

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 const express = require('express')
 const app = express()
 const routes = require('./routes/index')
-// const path = require('path')
+const path = require('path')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.use(express.static(__dirname + '/client/build/'))
 
-app.get('*', (req, res) => {
-    res.sendFile(__dirname + '/client/build/index.html')
-})
+// app.get('*', (req, res) => {
+//     res.sendFile(__dirname + '/client/build/index.html')
+// })
 
-// app.get('*', function (req, res) {
-//     const index = path.join(__dirname, 'build', 'index.html');
-//     res.sendFile(index);
-//   })
+app.get('*', function (req, res) {
+    const index = path.join(__dirname, 'build', 'index.html');
+    res.sendFile(index);
+  })
 
 app.use('/', routes)
 
