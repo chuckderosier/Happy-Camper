06b44b91ea95ea060c219b517e79dbfb912a8f83
chuckderosier <chuckderosier2018@gmail.com>
2019-05-16 15:22:10 -0400

    fixed i hope

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ app.use(express.json())
 
 app.use(express.static(__dirname + '/client/build/'))
 
-app.get('*', function (req, res) {
+// app.get('*', (req, res) => {
+//     res.sendFile(__dirname + '/client/build/index.html')
+// })
+
+app.get('/', function (req, res) {
     const index = path.join(__dirname, 'build', 'index.html');
     res.sendFile(index);
   })
